c04ee6c9468b79817a35c7c41722e5a19cae27e5
Ashu <mailtoashu478@gmail.com>
2023-06-29 13:18:12 +0530

    Added more Htmls file

---
+++ GIT Notes.php
@@ -0,0 +1,32 @@
+### NOTES:
+
+##GIT:-
+=>Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency. 
+
+
+
+
+
+Note:Git actualy keep record of all the changes done .
+=> In order to get the repo either we do "cloning or we initialize (init) it .
+
+=>To set The user name nad email:
+ git config --global user.name Ashu
+ git config --global user.email mailtoashu478@gmail.com
+ 
+ => to initilazie git repo 
+ git init
+
+
+=> to check status of all files:-
+git status 
+=> It will all fles status whether it is untracked ,unmodified,modified ,staged(finall commit)
+once we staged  any file then it wull go in git snapshot and git wil track of this file 
+
+## to add a single  file in staging area :
+git add index.html
+
+## To add more than one file:- git -A
+## to commit any  file : git commit  
+## to add any message while git commit without opening any editor to write the msg :git commit -m "Added more Htmls file"
+
