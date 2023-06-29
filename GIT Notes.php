fef65da156798fb80a1fe72bf4ed9ad236093b24
Ashu <mailtoashu478@gmail.com>
2023-06-29 14:15:57 +0530

    Adding without passing in staging area

--- GIT Notes.php
+++ GIT Notes.php
@@ -29,4 +29,23 @@ git add index.html
 ## To add more than one file:- git -A
 ## to commit any  file : git commit  
 ## to add any message while git commit without opening any editor to write the msg :git commit -m "Added more Htmls file"
+this "-m" flag is very much important command 
 
+## in order to match with last commit we can run command :git checkout
+
+#For Mutiples files, we can use "-f" flag: git checkout -f 
+=> it will match all our changes in any file with last commit (match the working directoy with last commit )
+
+# In order to check what commit ha sbeen performed: git log
+
+# In order to check only last 2 commit we can use flag "-p" : git log -p -2
+=> It will show all file also and changes : Now to quit form here hus enter "q"
+
+## In order to see ethe diferce between file done jdut run the command :git diff
+
+It compares  working directoy with staging area (check given and compare with it last edited file)  
+
+## to comapre "staging area" with "last commit" :git diff --stage
+
+## If we wan to add file directly into commit without sending in staging area :
+git commit -a -m "Adding without passing in staging area"
